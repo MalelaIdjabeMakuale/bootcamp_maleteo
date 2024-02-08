7295a7156a08969ad3afcdb8073b38b1fc1c13f5
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2024-02-08 08:05:53 +0100

    falloss

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { Component,OnInit } from '@angular/core';
+
 
 @Component({
-  selector: 'app-landing-page',
+  selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
-export class LandingPageComponent {
+export class LandingPageComponent { showModal = true;
+  showModal2 = false;
 
+
+  ngOnInit(): void {
+  }
+  closeModal(id:string) {
+    if (id === 'showModal' ){
+
+      this.showModal = false;
+      this.showModal2 = true;
+
+    }else{
+      this.showModal2 = false;
+    }
+  };
 }
