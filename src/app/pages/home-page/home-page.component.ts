7d0e470805d3495803ee24670c5e505370628f77
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2024-02-08 07:58:24 +0100

    para produccion

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  showModal = true;
+  showModal2 = false;
 
+
+  ngOnInit(): void {
+  }
+  closeModal(id:string) {
+    if (id === 'showModal' ){
+
+      this.showModal = false;
+      this.showModal2 = true;
+
+    }else{
+      this.showModal2 = false;
+    }
+  };
 }
