2bc3273cbae6fe8fa2f4103069a642f604121216
IamSergi96 <ergimataslargo123@gmail.com>
2024-02-08 23:51:16 +0100

    login, pero falta el app.module

--- src/app/pages/login-user-page/login-user-page.component.ts
+++ src/app/pages/login-user-page/login-user-page.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-user-page',
-  standalone: true,
-  imports: [],
   templateUrl: './login-user-page.component.html',
-  styleUrl: './login-user-page.component.css'
+  styleUrls: ['./login-user-page.component.css']
 })
-export class LoginUserPageComponent {
+export class LoginUserPageComponent implements OnInit {
+  loginForm: FormGroup = this.formbuilder.group({
+    username: ["", Validators.required],
+    password: ["", Validators.required]
+  });
 
+  registerForm: FormGroup = this.formbuilder.group({
+    username: ["", Validators.required],
+    nombre: ["", Validators.required],
+    apellidos: ["", Validators.required],
+    password: ["", Validators.required],
+    confirmPassword: ["", Validators.required]
+  });
+
+  showLoginForm: boolean = true;
+  showRegisterForm: boolean = false;
+
+  constructor(private formbuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.showLoginForm && this.loginForm.valid) {
+      this.router.navigate(['/usuarios']);
+    } else if (!this.showLoginForm && this.registerForm.valid) {
+      this.router.navigate(['/usuarios']);
+    }
+  }
+
+  toggleForm() {
+    this.showLoginForm = !this.showLoginForm;
+  }
 }
+
