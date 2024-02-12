f33fefd12cfb06a916e0249b2515be5dbfce7cf6
Robert Rodriguez Carton <robertrodriguezcarton@gmail.com>
2024-02-12 15:13:14 +0100

    arreglos malelaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
-=======
 import { HttpClient } from '@angular/common/http';
->>>>>>> origin/styles
 
 
 @Component({
