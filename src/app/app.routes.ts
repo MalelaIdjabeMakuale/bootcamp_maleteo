ecaebe5be0da890dbbe325a0b903ea8ea69e3014
IamSergi96 <ergimataslargo123@gmail.com>
2024-02-12 09:13:16 +0100

    anuncio formulario, queda hacer la lista de todos y el detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,16 +7,20 @@ import { UsersPageComponent } from './pages/users-page/users-page.component';
 import { DetailPagesComponent } from './pages/detail-pages/detail-pages.component';
 import { LocationPagesComponent } from './pages/location-pages/location-pages.component';
 import {LocalizacionComponent} from './pages/localizacion/localizacion.component';
-import { AdsPagesComponent } from './pages/ads-pages/ads-pages.component';
+import { AdsDetailComponent } from './pages/ads/ads-detail/ads-detail.component';
+import { AdsFormComponent } from './pages/ads/ads-form/ads-form.component';
+import { AdsListComponent } from './pages/ads/ads-list/ads-list.component';
 
 
 export const routes: Routes = [{path:'', component: LandingPageComponent},
 {path:'home', component: HomePageComponent},
-{path:'registro', component:LoginUserPageComponent},
+{path:'registro/usuario', component:LoginUserPageComponent},
 {path:'usuarios', component: UsersPageComponent},
 {path:'usuarios/:id', component: DetailPagesComponent}, 
 {path: 'ubicacion', component: LocationPagesComponent},
-{path: 'anuncios', component: AdsPagesComponent},
+{path: 'registro/anuncio', component: AdsFormComponent},
+{path: 'anuncios', component: AdsListComponent},
+{path: 'anuncios/:id', component: AdsDetailComponent},
 {path: 'localizacion', component: LocalizacionComponent}
 
     
---
+++ src/app/pages/ads/ads-detail/ads-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ads-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './ads-detail.component.html',
+  styleUrl: './ads-detail.component.css'
+})
+export class AdsDetailComponent {
+
+}
