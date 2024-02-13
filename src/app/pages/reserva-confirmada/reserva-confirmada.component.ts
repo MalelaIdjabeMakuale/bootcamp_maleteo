e598b0be2c24465cf9e216a1779b4dc5e5bee479
Carmendlcp <carmendelacallepinto@hotmail.com>
2024-02-13 17:16:03 +0100

    pantalla reserva confirmada

---
+++ src/app/pages/reserva-confirmada/reserva-confirmada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-confirmada',
+  standalone: true,
+  imports: [],
+  templateUrl: './reserva-confirmada.component.html',
+  styleUrl: './reserva-confirmada.component.css'
+})
+export class ReservaConfirmadaComponent {
+
+}
