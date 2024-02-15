dcf9e6a4cae41746a3dceeac42179cb6b942a2f8
bokombe <malela.im90@gmail.com>
2024-02-15 16:18:50 +0100

    borrar local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MDCSnackbar } from '@material/snackbar';
 
 
 @Component({
