bc75942f70059a13e04229917e5d6fbd52147287
IamSergi96 <ergimataslargo123@gmail.com>
2024-02-18 11:51:25 +0100

    funciona bien totdo ya

--- src/app/pages/ads/ads-form/ads-form.component.ts
+++ src/app/pages/ads/ads-form/ads-form.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ServicesService } from '../../../services/services.service';
-import swal from 'sweetalert';
 import { HttpClient } from "@angular/common/http";
 import { AuthenticateService } from '../../../services/authenticate.service';
 import { Map, marker, tileLayer, icon, LeafletMouseEvent } from 'leaflet';
@@ -18,10 +17,10 @@ export class AdsFormComponent implements OnInit {
   propertyType = ['Casa', 'Hotel', 'Establecimiento'];
   propertySpace = ['Habitación', 'Hall', 'Trastero', 'Buhardilla', 'Garaje'];
   selectedFile: File | null = null;
-  longitude: any;
-  latitude: any;
   isLoading = true;
   marker: any;
+  lat: any;
+  lon: any;
 
   anuncioForm: FormGroup = this.formbuilder.group({
     name: new FormControl(''),
@@ -43,7 +42,8 @@ export class AdsFormComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Verificar token al cargar la página
+    this.getLocation();
+
     const token = localStorage.getItem("token");
     console.log('Token de autenticación:', token);
 
@@ -55,12 +55,12 @@ export class AdsFormComponent implements OnInit {
         console.error('Error de autenticación', error);
         this.router.navigate(['/registro']);
       }
-      );
-      this.createMap();
+    );
   }
+
   createMap(): void {
     this.isLoading = true;
-    const map = new Map('map').setView([41.3851, 2.1734], 13);
+    const map = new Map('map').setView([this.lat, this.lon], 13);
     tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution:
@@ -68,36 +68,40 @@ export class AdsFormComponent implements OnInit {
     }).addTo(map);
     this.isLoading = false;
 
+    const customMarker = marker([this.lat, this.lon], {
+      icon: icon({
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+        iconUrl: '../../../assets/icons8-location-pin-32.png',
+      })
+    }).addTo(map);
+
+    const latitudeControl = this.anuncioForm.get('latitude');
+    const longitudeControl = this.anuncioForm.get('longitude');
+
+    if (latitudeControl && longitudeControl) {
+      latitudeControl.setValue(this.lat);
+      longitudeControl.setValue(this.lon);
+    }
+
     map.on('click', (e: LeafletMouseEvent) => {
-      this.latitude = e.latlng.lat;
-      this.longitude = e.latlng.lng;
+      this.lat = e.latlng.lat;
+      this.lon = e.latlng.lng;
 
-      if (this.marker) {
-        map.removeLayer(this.marker);
+      customMarker.setLatLng([this.lat, this.lon]);
+
+      if (latitudeControl && longitudeControl) {
+        latitudeControl.setValue(this.lat);
+        longitudeControl.setValue(this.lon);
       }
-      this.marker = marker([this.latitude, this.longitude], {
-        icon: icon({
-          iconSize: [25, 41],
-          iconAnchor: [13, 41],
-          iconUrl: 'leaflet/marker-icon.png',
-          shadowUrl: 'leaflet/marker-shadow.png'
-        })
-      }).addTo(map);
-      const latitudeControl = this.anuncioForm.get('latitude');
-const longitudeControl = this.anuncioForm.get('longitude');
-
-if (latitudeControl && longitudeControl) {
-  latitudeControl.setValue(this.latitude);
-  longitudeControl.setValue(this.longitude);
-}
-
     });
   }
+
   async onSubmit() {
     if (this.anuncioForm.valid) {
       const formValue = this.anuncioForm.value;
 
-      this.isLoading = true; // Iniciar la carga
+      this.isLoading = true; 
 
       this.servicesService.registerLocker(formValue).subscribe(
         (response) => {
@@ -105,7 +109,7 @@ if (latitudeControl && longitudeControl) {
 
           const userId = localStorage.getItem('id_user');
           const locker = response.estacion._id;
-          const lockerUpdate = { estaciones: locker};
+          const lockerUpdate = { estaciones: locker };
 
           this.servicesService.updateUser(userId, lockerUpdate).subscribe(
             (response) => {
@@ -124,95 +128,19 @@ if (latitudeControl && longitudeControl) {
           this.anuncioForm.enable();
         }
       ).add(() => {
-        this.isLoading = false; // Finalizar la carga
+        this.isLoading = false;
       });
     }
   }
 
-  // onFileSelected(event: any) {
-  //   this.selectedFile = event.target.files[0];
-  // }
-
-  // uploadFile() {
-  //   if (!this.selectedFile) {
-  //     return;
-  //   }
-
-  //   const formData = new FormData();
-  //   formData.append('file', this.selectedFile);
-
-  //   this.http.post<any>('http://api-plum-six.vercel.app/api/upload', formData).subscribe(
-  //     (response: any) => {
-  //       console.log('Imagen subida con éxito:', response.imageUrl);
-  //       this.anuncioForm.patchValue({
-  //         img: response.imageUrl
-  //       });
-  //     },
-  //     (error: any) => {
-  //       console.error('Error al subir la imagen:', error);
-  //     }
-  //   );
-  // }
+  getLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+        this.createMap();
+      });
+    }
+  }
 }
 
-
-//   const direccion = this.anuncioForm.get('location')?.value;
-//   const direccionCodificada = encodeURIComponent(direccion);
-//   const nominatimAPI = "https://nominatim.openstreetmap.org/search?format=json&limit=1&q=";
-
-//   try {
-//     const response = await fetch(nominatimAPI + direccionCodificada);
-//     const data = await response.json();
-//     console.log(data);
-
-//     if (data.length > 0) {
-//       const latitude2 = data[0].lat;
-//       const longitude2 = data[0].lon;
-
-//       this.anuncioForm.patchValue({
-//         latitude: latitude2,
-//         longitude: longitude2
-//       });
-
-//       const formValue = this.anuncioForm.value;
-
-//       this.servicesService.registerLocker({
-//         name: formValue.name,
-//         latitude: formValue.latitude,
-//         longitude: formValue.longitude,
-//         propertyType: formValue.propertyType,
-//         propertySpace: formValue.propertySpace,
-//         capacity: formValue.capacity,
-//         img: formValue.img,
-//         available: formValue.available,
-//       }).subscribe(
-//         response => {
-//           console.log('Datos enviados con exito a la API', response);
-//           console.log(response);
-
-//           this.router.navigate(['/anuncios']);
-//         },
-//         error => {
-//           console.error('Error al enviar datos a la API:', error);
-//         }
-//       );
-//     } else {
-//       console.log('Sin resultados para la dirección proporcionada');
-//     }
-//   } catch (error) {
-//     console.error('Error al obtener datos de Nominatim', error);
-
-
-  //   this.http.post<any>('http://api-plum-six.vercel.app/api/upload', formData).subscribe(
-  //     (response: any) => {
-  //       console.log('Imagen subida con éxito:', response.imageUrl);
-  //       this.anuncioForm.patchValue({
-  //         img: response.imageUrl
-  //       });
-  //     },
-  //     (error: any) => {
-  //       console.error('Error al subir la imagen:', error);
-  //     }
-  //   );
-  // }
-// }
